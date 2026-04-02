b28b1f1e27d0accd643cd0711a42b7a83b122f61
aem1ca <emkrilai@gmail.com>
2026-04-02 13:30:21 +0300

    Pause menu works
    yay

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -9,12 +9,14 @@ public class GameUIManager : MonoBehaviour
     public GameObject pauseMenu;
 
     private bool isPaused = false;
+    private bool gameIsActive = false;
 
     void Start()
     {
         deathScreen.SetActive(false);
         winScreen.SetActive(false);
         pauseMenu.SetActive(false);
+        gameIsActive = true;
     }
 
     void Update()
@@ -27,18 +29,21 @@ public class GameUIManager : MonoBehaviour
 
     public void ShowDeathScreen()
     {
+        gameIsActive = false;
         deathScreen.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void ShowWinScreen()
     {
+        gameIsActive = false;
         winScreen.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void TogglePause()
     {
+        if (!gameIsActive) return;
         isPaused = !isPaused;
         pauseMenu.SetActive(isPaused);
         Time.timeScale = isPaused ? 0f : 1f;
@@ -61,5 +66,10 @@ public class GameUIManager : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    public void OnPauseButton()
+    {
+        TogglePause();
     }
 }
