5132d92642cbd84f9e141bce0127a86f8d77a29b
aem1ca <emkrilai@gmail.com>
2026-04-02 04:25:33 +0300

    MORE UI IT WORKS
    HAIL MARY IT WORKS THE BUTTONS WORK

--- Assets/UI stuff/UIManagerTWO.cs
+++ Assets/UI stuff/UIManagerTWO.cs
@@ -11,8 +11,17 @@ public class UIManagerTWO : MonoBehaviour
     public GameObject winScreen;
     public GameObject settingsScreen;
 
+    [Header("Tutorial")]
+    public GameObject tutorialScreen;
+    public Image tutorialImage;
+    public Sprite[] tutorialSlides;
+
     [Header("Audio")]
     public AudioSource menuMusic;
+
+    [Header("SFX")]
+    public AudioSource sfxSource;
+    public AudioClip buttonClick;
 
     [Header("Mute Button")]
     public Sprite muteSprite;
@@ -20,6 +29,7 @@ public class UIManagerTWO : MonoBehaviour
     public Image muteButtonImage;
 
     private bool isPaused = false;
+    private int currentSlide = 0;
 
     void Start()
     {
@@ -34,6 +44,12 @@ public class UIManagerTWO : MonoBehaviour
         }
     }
 
+    // --- Helper ---
+    void PlayClick()
+    {
+        sfxSource.PlayOneShot(buttonClick);
+    }
+
     // --- Start Screen ---
     public void ShowStartScreen()
     {
@@ -42,14 +58,48 @@ public class UIManagerTWO : MonoBehaviour
         pauseMenu.SetActive(false);
         winScreen.SetActive(false);
         settingsScreen.SetActive(false);
+        tutorialScreen.SetActive(false);
     }
 
     public void OnStartButton()
     {
-        startScreen.SetActive(false);
-        settingsScreen.SetActive(false);
+        Debug.Log("Start button pressed");
+        Debug.Log("tutorialScreen: " + tutorialScreen);
+        Debug.Log("tutorialSlides length: " + tutorialSlides.Length);
+        Debug.Log("tutorialImage: " + tutorialImage);
+    
+        PlayClick();
         menuMusic.Stop();
+        startScreen.SetActive(false);
+        currentSlide = 0;
+        tutorialScreen.SetActive(true);
+        Debug.Log("tutorialScreen active: " + tutorialScreen.activeSelf);
+        tutorialImage.sprite = tutorialSlides[currentSlide];
+        Debug.Log("Slide set to: " + tutorialSlides[currentSlide].name);
+    }
+
+    // --- Tutorial ---
+    public void OnTutorialNextButton()
+    {
+        PlayClick();
+        currentSlide++;
+
+        if (currentSlide >= tutorialSlides.Length)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            tutorialImage.sprite = tutorialSlides[currentSlide];
+        }
+    }
+
+    public void OnTutorialSkipButton()
+    {
+        PlayClick();
         Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
     }
 
     // --- Death & Win ---
@@ -68,23 +118,27 @@ public class UIManagerTWO : MonoBehaviour
     // --- Shared Buttons ---
     public void OnRetryButton()
     {
+        PlayClick();
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void OnMainMenuButton()
     {
+        PlayClick();
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 
     public void OnSettingsButton()
     {
+        PlayClick();
         settingsScreen.SetActive(true);
     }
 
     public void OnSettingsBackButton()
     {
+        PlayClick();
         settingsScreen.SetActive(false);
     }
 
@@ -98,6 +152,7 @@ public class UIManagerTWO : MonoBehaviour
 
     public void OnResumeButton()
     {
+        PlayClick();
         isPaused = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
@@ -105,12 +160,14 @@ public class UIManagerTWO : MonoBehaviour
 
     public void OnQuitButton()
     {
+        PlayClick();
         Application.Quit();
     }
 
     // --- Mute ---
     public void OnMuteButton()
     {
+        PlayClick();
         menuMusic.mute = !menuMusic.mute;
         muteButtonImage.sprite = menuMusic.mute ? muteSprite : unmuteSprite;
     }
